82d8480b4fce5b098939585f12694f1ff72ef35e
Zain ul Abideen <zk438256@gmail.com>
2025-11-30 23:42:33 +0500

    final push

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,22 +1,21 @@
-import "../config/env.config.js";
+import "./config/env.config.js";
 import type { Request, Response, NextFunction } from "express";
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import helmet from "helmet";
 import morgan from "morgan";
-import connectDB from "../db/db.js";
-import { errorHandler } from "../middlewares/errorHandler.middleware.js";
-import userRoutes from "../routes/user.routes.js";
-import farmerRoutes from "../routes/farmer.routes.js";
-import publicProductRoutes from "../routes/publicProduct.routes.js";
-import cartRoutes from "../routes/cart.routes.js";
-import paymentRoutes from "../routes/payment.routes.js";
-import uploadRoutes from "../routes/upload.routes.js";
-import reviewRoutes from "../routes/review.routes.js";
-import { stripeWebhook } from "../controllers/stripeWebhook.controller.js";
+import connectDB from "./db/db.js";
+import { errorHandler } from "./middlewares/errorHandler.middleware.js";
+import userRoutes from "./routes/user.routes.js"
+import farmerRoutes from "./routes/farmer.routes.js";
+import publicProductRoutes from "./routes/publicProduct.routes.js";
+import cartRoutes from "./routes/cart.routes.js";
+import paymentRoutes from "./routes/payment.routes.js";
+import uploadRoutes from "./routes/upload.routes.js";
+import reviewRoutes from "./routes/review.routes.js";
+import { stripeWebhook } from "./controllers/stripeWebhook.controller.js";
 import serverless from "serverless-http";
-
 const app = express();
 
 
@@ -27,36 +26,33 @@ async function initDB() {
     isDBConnected = true;
   }
 }
+const PORT = process.env.PORT || 5000;
+
+
 
 
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
-
 app.use(cookieParser());
-
-
 app.post(
   "/api/webhooks/stripe",
   express.raw({ type: "application/json" }),
   stripeWebhook
 );
-
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-
 app.use(helmet());
 app.use(morgan("dev"));
-
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-  if (err instanceof SyntaxError && "body" in err) {
-    return res.status(400).json({
-      success: false,
-      message: "Invalid JSON",
-    });
-  }
-  next(err);
+    if (err instanceof SyntaxError && "body" in err) {
+        return res.status(400).json({ 
+            success: false, 
+            message: "Invalid JSON" 
+        });
+    }
+    next(err);
 });
+
+
 
 app.use("/api/users", userRoutes);
 app.use("/api/farmers", farmerRoutes);
@@ -65,12 +61,16 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/payment", paymentRoutes);
 app.use("/api/upload", uploadRoutes);
 app.use("/api", reviewRoutes);
-
 app.get("/", (_req, res) => res.json({ status: "OK" }));
-
 app.use(errorHandler);
 
-export const handler = serverless(async (req, res) => {
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
+
+
+export const handler = serverless(async (req:Request, res:Response) => {
   await initDB(); // ensure DB connection BEFORE handling requests
   return app(req, res);
 });
